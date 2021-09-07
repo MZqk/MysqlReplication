c7fdaa2afd606d7dc0b72abbc7fa691734d4f2b8
zhangqiankun <zhangqiankun@guandata.com>
2021-09-07 11:00:04 +0800

    set all host connect

--- changepasswd.sh
+++ changepasswd.sh
@@ -8,3 +8,5 @@ echo "mysql -uroot -p'${Mysqlroot}'"
 
 echo "Change Mysql root password"
 mysql -uroot -p$Mysqlroot --connect-expired-password  <<< "set password for root@localhost = password('mysql1qaz@WSX');"
+mysql -uroot -pmysql1qaz@WSX  <<< "update mysql.user set host='%' where host='localhost' and user='root';"
+systemctl restart mysql
