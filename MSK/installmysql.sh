37a1b83bff23ecd301af15933b47769a2ef6ccca
MZZZ <zqk_mz@163.com>
2021-09-12 21:50:58 +0800

    Update installmysql.sh

--- MSK/installmysql.sh
+++ MSK/installmysql.sh
@@ -3,3 +3,6 @@
 /bin/rpm -e $(/bin/rpm -qa | grep mariadb|xargs) --nodeps
 
 rpm -ivh mysql-community-server-5.7.34-1.el7.x86_64.rpm mysql-community-common-5.7.34-1.el7.x86_64.rpm mysql-community-client-5.7.34-1.el7.x86_64.rpm mysql-community-libs-5.7.34-1.el7.x86_64.rpm mysql-community-libs-compat-5.7.34-1.el7.x86_64.rpm
+
+systemctl start mysqld
+systemctl stop mysqld
