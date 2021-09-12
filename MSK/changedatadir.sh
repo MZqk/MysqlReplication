7bf197307a54d2ffba91f9f0adf8a1bc70c6a54a
MZZZ <zqk_mz@163.com>
2021-09-12 22:09:44 +0800

    Create changedatadir.sh

---
+++ MSK/changedatadir.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+datadir=`cat mysql.param |grep datadir|awk -F '=' '{print $2}'`
+if [ -e $datadir ] 
+then
+  echo "mysql data directory exists"
+else
+  mkdir -p $datadir
+  echo "create mysql data directory "
+fi
