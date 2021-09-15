03d0e063c3d4494d8887b11fb18b80a6a177b76f
MZZZ <zqk_mz@163.com>
2021-09-15 09:53:28 +0800

    Update uninstallmysql.sh

--- MSK/uninstallmysql.sh
+++ MSK/uninstallmysql.sh
@@ -5,6 +5,8 @@ systemctl stop mysqld
 #/bin/rpm -e $(/bin/rpm -qa | grep mysql|xargs) --nodeps
 yum remove -y mysql-community-server-5.7.34-1.el7.x86_64 mysql-community-common-5.7.34-1.el7.x86_64 mysql-community-client-5.7.34-1.el7.x86_64 mysql-community-libs-5.7.34-1.el7.x86_64 mysql-community-libs-compat-5.7.34-1.el7.x86_64 
 
-rm -rf /var/lib/mysql
 rm -rf /etc/my.conf
 rm -rf /var/log/mysql*
+datadir=`cat mysql.param |grep datadir|awk -F '=' '{print $2}'`
+rm -rf /var/lib/mysql
+rm -rf $datadir
